3a63d7c1806daa13f4f8bb310b9dbc85c357b39e
Pedro Esteves <pedroesteves2803@gmail.com>
2023-07-20 01:11:11 -0300

    Criar rotas de list e create

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Estacaionamento\EstacionamentoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/estacionamento', [EstacionamentoController::class, 'index']);
+Route::post('/estacionamento', [EstacionamentoController::class, 'store']);
