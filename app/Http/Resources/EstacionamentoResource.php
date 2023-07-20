6f447422611e48683ee761eb208f0bdf68c9195e
Pedro Esteves <pedroesteves2803@gmail.com>
2023-07-20 01:10:36 -0300

    Criar resource para padronizar respostas

---
+++ app/Http/Resources/EstacionamentoResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EstacionamentoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'nome' => $this->nome,
+            'quantidadeDeVagas' => $this->quantidadeDeVagas,
+            'ativo' => $this->ativo,
+            'criado' => Carbon::make($this->created_at)->format('d-m-Y'),
+        ];
+    }
+}
