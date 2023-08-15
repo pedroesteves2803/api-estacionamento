424a4257766fcc3bcd0b3634000d812891f01897
Pedro Esteves <pedroesteves2803@gmail.com>
2023-08-15 01:42:53 -0300

    Criar DTO para erros

---
+++ app/Dtos/Error/ErrorDTO.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Dtos\Error;
+
+use App\Dtos\AbstractDTO;
+use App\Dtos\InterfaceDTO;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\Rule;
+
+class ErrorDTO extends AbstractDTO implements InterfaceDTO
+{
+    public function __construct(
+        public readonly string $message,
+    )
+    {
+        $this->validate();
+    }
+
+    public function rules():array
+    {
+        return [
+            'message' => [
+                'required',
+            ],
+        ];
+
+    }
+
+    public function messages():array{
+        return [];
+    }
+
+    public function validator(): Validator{
+        return validator($this->toArray(), $this->rules(), $this->messages());
+    }
+
+    public function validate():array{
+        return $this->validator()->validate();
+    }
+}
