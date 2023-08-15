f1d0e5a874ad490b9b9a501db98ec55dbe36f240
Pedro Esteves <pedroesteves2803@gmail.com>
2023-08-15 01:42:25 -0300

    Alterar carrros

---
+++ database/migrations/2023_07_23_232331_create_cars_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('plate')->unique();
+            $table->string('model');
+            $table->string('color');
+            $table->dateTime('input')->default(now());
+            $table->dateTime('output')->nullable();
+            $table->foreignId('parking_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
