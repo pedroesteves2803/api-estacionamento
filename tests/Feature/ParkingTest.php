95951fbd2b3bbc1b4ee40d6954644f7b12811246
Pedro Esteves <desenv3.ti@apcdcentral.com.br>
2023-09-27 01:28:25 -0300

    Adicionar teste

---
+++ tests/Feature/ParkingTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class ParkingTest extends TestCase
+{
+    protected $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutMiddleware(\Illuminate\Auth\Middleware\Authenticate::class);
+
+        $this->user = User::factory()->create();
+    }
+
+    public function testGetParkingsAuthenticate(): void
+    {
+        $response = $this->actingAs($this->user)->get('/api/parking');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [],
+            ]);
+    }
+
+    public function testGetParkingsNoAuthenticate(): void
+    {
+        $response = $this->get('/api/parking');
+
+        $response->assertStatus(401)
+            ->assertJson([
+                'message' => 'Unauthenticated',
+            ]);
+    }
+}
